5b8174b971be55bce5546587e8a9f8e9ed8a2d71
Anatoly Kulikov <kulikovaa@anodialog.ru>
2023-02-15 01:28:10 +0300

    Update docker-compose and add Dockerfile for laravel

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['hello, world!', 'hello, world 2!'];
+    return ['hello, world!', 'hello, world2!'];
+});
+
+
+Route::get('/checkdb', function () {
+    try {
+        DB::connection()->getPdo();
+        if(DB::connection()->getDatabaseName()){
+            echo "Yes! Successfully connected to the DB: " . DB::connection()->getDatabaseName();
+        }else{
+            die("Could not find the database. Please check your configuration.");
+        }
+    } catch (\Exception $e) {
+        die("Could not open connection to database server.  Please check your configuration.");
+    }
 });
